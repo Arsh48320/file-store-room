6a5b6f6a9c3cc77757cf11176d2fe614e242e5fb
AN code <arshnehal03@gmail.com>
2024-12-05 13:58:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php starting</title>
+</head>
+<body>
+<?php
+    echo "Hello php this is printed using echo in php";
+?>
+<div class="container">
+    This is my first php website
+</div>
+</body>
+</html>
